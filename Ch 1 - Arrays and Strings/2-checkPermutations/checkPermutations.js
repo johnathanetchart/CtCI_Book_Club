5a0a2b239ccaf7ab79b95d3e80eeb2e395944a06
Johnathan Etchart <johnathan.etchart@gmail.com>
2021-04-24 15:49:08 -0700

    finished check permutations

--- Ch 1 - Arrays and Strings/2-checkPermutations/checkPermutations.js
+++ Ch 1 - Arrays and Strings/2-checkPermutations/checkPermutations.js
@@ -1,7 +1,42 @@
 //Given two strings, write a method to decide if one is a permutation of the other.
 
+//just checking if the strings have all the same letters and the same amounts of each
+
+
+
 const checkPermutations = (str1, str2) => {
-  //Please code here
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  let str1Obj = createObj(str1);
+  let str2Obj = createObj(str2);
+
+  for (let key in str1Obj) {
+    if (str1Obj[key] !== str2Obj[key]) {
+      return false;
+    }
+  }
+
+  for (let key in str2Obj) {
+    if (str1Obj[key] !== str2Obj[key]) {
+      return false;
+    }
+  }
+
+  return true;
 };
+
+const createObj = (str) => {
+  let obj = {};
+  for (let i = 0; i < str.length; i++) {
+    if (obj[i] === undefined) {
+      obj[i] = 1;
+    } else {
+      obj[i]++;
+    }
+  }
+  return obj;
+}
 
 module.exports = checkPermutations;
