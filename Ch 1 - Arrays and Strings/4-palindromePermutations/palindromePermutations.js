0eab900bdeb4f1cc544ed9ea508f804b13bedd32
Johnathan Etchart <johnathan.etchart@gmail.com>
2021-04-24 16:14:07 -0700

    finished palindrome permutations

--- Ch 1 - Arrays and Strings/4-palindromePermutations/palindromePermutations.js
+++ Ch 1 - Arrays and Strings/4-palindromePermutations/palindromePermutations.js
@@ -2,6 +2,34 @@
 
 function palindromePermutations(str) {
   //Coding here is optional, but recommended.
-}
+  //a palindrome has even numbers of characters, one on each side of the middle
+  //if it's an odd length, then a single character can appear an odd number of times
+  str = str.toLowerCase();
+  let charCount = {};
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== ' ') {
+      if (charCount[str[i]] === undefined) {
+        charCount[str[i]] = 1;
+      } else {
+        charCount[str[i]]++;
+      }
+    }
+  }
+  let odds = 0;
+  for (let key in charCount) {
+    if(charCount[key] % 2 === 1) {
+      odds++;
+    }
+  }
+  if (odds <= 1) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const isPalindrome = () => {
+  //might need a checker, but building one might be fine
+};
 
 module.exports = palindromePermutations;
