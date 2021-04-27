f39532f3dcaced438ff92b4828e4485819d4a97f
Johnathan Etchart <johnathan.etchart@gmail.com>
2021-04-27 11:52:18 -0700

    finished rotateMatrix, not while in place

--- Ch 1 - Arrays and Strings/7-rotateMatrix/rotateMatrix.js
+++ Ch 1 - Arrays and Strings/7-rotateMatrix/rotateMatrix.js
@@ -1,7 +1,24 @@
 //Given an image represented by an N x N matrix, where each pixel in the image is represented by an integer, write a method to rotate the image by 90 degrees. Can you do this in place?
 
 function rotateMatrix(matrix) {
-  //EVERYTHING MUST ROTATE!
+  //not in place
+  let i = matrix.length - 1;
+  let j = 0;
+  let result = [];
+  let row = [];
+  while(j < matrix[0].length) {
+
+    while(i >= 0) {
+      row.push(matrix[i][j])
+      i--;
+    }
+    result.push(row);
+    row = [];
+    j++;
+    i = matrix[0].length - 1;
+  }
+  console.log(result);
+  return result;
 }
 
 module.exports = rotateMatrix;
