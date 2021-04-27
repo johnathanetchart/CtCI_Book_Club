2c0aa7f3982ba2e19d7f2d232ec857c6c940f0da
Johnathan Etchart <johnathan.etchart@gmail.com>
2021-04-27 11:29:10 -0700

    finished stringCompression

--- Ch 1 - Arrays and Strings/6-stringCompression/stringCompression.js
+++ Ch 1 - Arrays and Strings/6-stringCompression/stringCompression.js
@@ -1,7 +1,22 @@
 //Implement a method to perform basic string compression using the counts of repeated characters. For example, the string aabcccccaaa would become a2blc5a3. If the "compressed" string would not become smaller than the original string, your method should return the original string. You can assume the string has only uppercase and lowercase letters (a - z).
 
 function stringCompression(str) {
-  //I'd put the code here
+  let result = '';
+  let compressible = false;
+  let count;
+  for(let i = 0; i < str.length; i++) {
+    count = 1;
+    while(str[i] === str[i + 1]) {
+      compressible = true;
+      count++;
+      i++;
+    }
+    result += (str[i] + count);
+  }
+  if(result.length < str.length) {
+    return result;
+  }
+  return str;
 }
 
 module.exports = stringCompression;
